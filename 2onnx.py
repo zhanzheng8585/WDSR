b0b79117efb8d5d8f8278c29febcf9c2089d6dd0
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-02-16 15:49:18 -0500

    Update 2onnx.py

--- 2onnx.py
+++ 2onnx.py
@@ -23,6 +23,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage.color import lab2rgb
 from skimage import io
+import onnxruntime
 
 # import prune_util
 # from prune_util import GradualWarmupScheduler
@@ -96,16 +97,38 @@ def main():
                               verbose=False)
     print("Export of torch_model.onnx complete!")
 
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
 def check():
 
-    # Load the ONNX model
-    model = onnx.load("wdsr_b.onnx")
+    args = get_args()
+    model = MODEL(args)
+    checkpoint = torch.load("./checkpoint/checkpoint6/model_epoch133_step1.pth")
+    model.load_state_dict(checkpoint["model"].state_dict())
 
-    # Check that the IR is well formed
-    onnx.checker.check_model(model)
+    ort_session = onnxruntime.InferenceSession("wdsr_b.onnx")
 
-    # Print a human readable representation of the graph
-    onnx.helper.printable_graph(model.graph)
+    x = torch.randn(1, 3, 256, 256, requires_grad=True)
+    torch_out = model(x)
+    # # Load the ONNX model
+    # model = onnx.load("wdsr_b.onnx")
+
+    # # Check that the IR is well formed
+    # onnx.checker.check_model(model)
+
+    # # Print a human readable representation of the graph
+    # onnx.helper.printable_graph(model.graph)
+
+    # compute ONNX Runtime output prediction
+    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+    ort_outs = ort_session.run(None, ort_inputs)
+
+    # compare ONNX Runtime and PyTorch results
+    np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
+
+    print("Exported model has been tested with ONNXRuntime, and the result looks good!")
 
 if __name__ == '__main__':
     main()
